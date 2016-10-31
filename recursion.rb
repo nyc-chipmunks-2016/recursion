f1958869dd86a0fe65bc41fcdb367555a8a417f0
lorelei522 <lorelei522@gmail.com>
2016-10-30 23:29:15 -0400

    create a palindrome? method using recursion

--- recursion.rb
+++ recursion.rb
@@ -1,8 +1,27 @@
-def palindrome?
+#must read the same backwards and forward
+	#string ==  string.reverse
+	#string[1..-2]	will keep running on the inside of the word
 
+def palindrome?(string)
+  return true if string.length == 0 || string.length == 1
+
+  if string[0] == string[-1]
+    palindrome?(string[1..-2])
+  else
+    false
+  end
 end
+
+p palindrome?("racecar")
+p palindrome?("lemon")
+
 
 
 def recursive_flatten
 
 end
+
+
+
+
+
